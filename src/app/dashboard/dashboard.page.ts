2ea922acb270d707b332af980c55f0bdb3e87cf4
Francis Miah <francis@axoninfosystems.com>
2019-07-30 02:10:09 +0000

    jc

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { MenuController, Events } from '@ionic/angular';
 import { ApisService } from '../services/apis.service';
 import { StorageService } from '../services/storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,12 +10,51 @@ import { StorageService } from '../services/storage.service';
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
-
-  constructor(private apisService: ApisService,
-              private storageService: StorageService, public menuCtrl: MenuController) { }
+  farmers: any[];
+  farms: any[];
+  requests: any[];
+  seasons: any[];
+  total: number;
+  constructor(private router: Router, private storageService: StorageService,
+              public menuCtrl: MenuController, public events: Events, private zone: NgZone) {
+  }
 
   ngOnInit() {
     this.menuCtrl.enable(true);
+    const self = this;
+    this.getFarms();
+    this.getFarmers();
+    this.getRequests();
+    this.getSeasons();
   }
 
+  async getFarms() {
+    await this.storageService.getKeyValue('farms').then(
+      data => {
+        this.farms = data;
+      }
+    );
+  }
+  async getFarmers() {
+    await this.storageService.getKeyValue('farmers').then(
+      data => {
+        this.farmers = data;
+      }
+    );
+  }
+
+  async getRequests() {
+    await this.storageService.getKeyValue('requests').then(
+      data => {
+        this.requests = data;
+      }
+    );
+  }
+  async getSeasons() {
+    await this.storageService.getKeyValue('seasons').then(
+      data => {
+        this.seasons = data;
+      }
+    );
+  }
 }
